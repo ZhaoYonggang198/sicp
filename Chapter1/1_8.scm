2bb93e90b6bd4afc707299e4a438f9798e690f02
Zhao Yonggang <yonggang.zhao@icloud.com>
2016-02-03 16:00:22 +0800

    cube added

--- Chapter1/1_8.scm
+++ Chapter1/1_8.scm
@@ -1,21 +1,24 @@
-(define (sqrt-iter guess x)
+(define (cube-iter guess x)
 	(if (good-enough? guess x)
 		guess
-		(sqrt-iter (improve guess x)
+		(cube-iter (improve guess x)
 			x)))
 
 (define (improve guess x)
-	(average guess (/ x guess)))
-
-(define (average x y)
-	(/ (+ x y) 2))
+	(/ 
+		(+ 
+			(/ x
+				(* guess guess))
+			(* 2 guess))
+		3))
 
 (define (good-enough? guess x)
 	(< (/ (abs (- guess
 				(improve guess x)))
 		guess)
 	0.0001))
+	
 
-(define (sqrt x)
-	(sqrt-iter 1.0 x))
+(define (cube x)
+	(cube-iter 1.0 x))
 
